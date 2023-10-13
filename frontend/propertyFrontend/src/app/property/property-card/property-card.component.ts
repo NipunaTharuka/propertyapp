e0d52898bdac96eb900ae3da4c8b838bd0f81b34
NipunaTharuka <narandeniya.nipuna@gmail.com>
2023-10-13 20:14:27 +0530

    first commit

---
+++ frontend/propertyFrontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,22 @@
+import { style } from "@angular/animations";
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-property-card',
+    //template: `<h1>I am card </h1>`,
+    templateUrl: 'property-card.component.html',
+    //styles: ['h1 {font-weight: normal;}']
+    styleUrls: ['property-card.component.css']
+}
+
+)
+export class PropertyCardComponent {
+
+    Property: any = {
+        "Id": 1,
+        "Name": "Rotar",
+        "Type": "House",
+        "Price": "1200"
+    }
+}
---
+++ frontend/propertyFrontend/src/app/typed.ts
@@ -0,0 +1,5 @@
+function Sum(a:number, b:number){
+    return a+b;
+}
+
+Sum(2,3);
